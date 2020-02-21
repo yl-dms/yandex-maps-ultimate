2cf522fdd5ca9c1b5c51d017ccc2cb2297e56401
Irbis888 <maxxka@outlook.com>
2020-02-21 23:57:39 +0700

    Базовая версия

---
+++ otl10.py
@@ -0,0 +1,37 @@
+import os
+import sys
+
+import pygame
+import requests
+api_server = "http://static-maps.yandex.ru/1.x/"
+
+lon = "37.530887"
+lat = "55.703118"
+delta = "0.002"
+
+params = {
+    "ll": ",".join([lon, lat]),
+    "spn": ",".join([delta, delta]),
+    "l": "map"
+}
+response = requests.get(api_server, params=params)
+
+if not response:
+    print("Ошибка выполнения запроса:")
+    print(api_server)
+    print("Http статус:", response.status_code, "(", response.reason, ")")
+    sys.exit(1)
+
+map_file = "map.png"
+with open(map_file, "wb") as file:
+    file.write(response.content)
+
+pygame.init()
+screen = pygame.display.set_mode((600, 450))
+screen.blit(pygame.image.load(map_file), (0, 0))
+pygame.display.flip()
+while pygame.event.wait().type != pygame.QUIT:
+    pass
+pygame.quit()
+
+os.remove(map_file)
